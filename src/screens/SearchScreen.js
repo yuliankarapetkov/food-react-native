87ae07b8ae11f002f483a68ab7e043be5bc317a7
Yulian Karapetkov <yulian.karapetkov@gmail.com>
2020-10-16 17:25:19 +0300

    feat: get a list of restaurants from Yelp

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -3,17 +3,33 @@ import { View, Text, StyleSheet } from 'react-native';
 
 import SearchBar from '../components/SearchBar';
 
+import yelp from '../api/yelp';
+
 const SearchScreen = () => {
     const [query, setQuery] = useState('');
+    const [restaurants, setRestaurants] = useState([]);
+
+    const searchRestaurants = async () => {
+        const { data: { businesses } } = await yelp.get('/search', {
+            params: {
+                limit: 50,
+                term: query,
+                location: 'san jose'
+            }
+        });
+
+        setRestaurants(businesses);
+    }
 
     return (
         <View>
             <SearchBar
                 query={query}
                 onChange={value => setQuery(value)}
-                onSubmit={() => console.log('submitted')}
+                onSubmit={searchRestaurants}
             />
             <Text>{query.length ? `Search for '${query}'..` : 'Type in something to start searching'}</Text>
+            <Text>We have found {restaurants.length} that match your search.</Text>
         </View>
     );
 };
